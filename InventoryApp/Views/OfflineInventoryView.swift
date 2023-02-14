ad208abc9870a30dafd89e8fe556282ecb5be18b
Patrick Coleman <patrick.coleman@teambespin.us>
2023-02-13 23:55:04 -0600

    Adds Inventory List View to OfflineInventoryView

--- InventoryApp/Views/OfflineInventoryView.swift
+++ InventoryApp/Views/OfflineInventoryView.swift
@@ -17,47 +17,102 @@ struct OfflineInventoryView: View {
 
     private var items: FetchedResults<Item>
 
-    var body: some View {
-        ZStack {
-            BackgroundView()
-            HStack {
-                if items.isEmpty {
-                    Text("Add an Inventory")
-                } else {
-                    List {
-                        ForEach(items) { item in
-                            let num = items.firstIndex(of: item)
-                            NavigationLink {
-                                Image(systemName: "plus")
-                            } label: {
-                                Text("Inventory #\(num!)")
-                            }
-                        }
-                        .onDelete(perform: deleteItems)
-                    }
-                    .scrollContentBackground(.hidden)
-                }
-            }
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button {
-                        addItem()
-                    } label: {
-                        Image(systemName: "plus")
-                            .foregroundColor(.white)
-                            .padding()
-                            .background(secondaryLightColor)
-                            .clipShape(Circle())
-                    }
-                    .tint(secondaryLightColor)
-                    .shadow(radius: 3)
-                    .padding(.vertical, 50)
-                    .padding(.horizontal, 40)
-                }
-            }
+    var floatingActionButton: some View {
+        Button {
+            addItem()
+        } label: {
+            Image(systemName: "plus")
+                .foregroundColor(.white)
+                .padding()
+                .background(secondaryLightColor)
+                .clipShape(Circle())
         }
+        .tint(secondaryLightColor)
+        .shadow(radius: 3)
+        .padding(.vertical, 50)
+        .padding(.horizontal, 40)
+    }
+
+    var emptyInventoryView: some View {
+        VStack {
+            Spacer()
+            ZStack {
+                primaryDarkColor.opacity(0.3)
+                    .frame(height: 100)
+                    .cornerRadius(15)
+                    .shadow(radius: 10)
+                Text("No Inventories to Show")
+                    .shadow(radius: 3)
+            }
+            Spacer()
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.vertical, 30)
+    }
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                BackgroundView()
+                HStack {
+                    if items.isEmpty {
+                        emptyInventoryView
+                    } else {
+                        List {
+                            ForEach(items) { item in
+                                let num: Int? = items.firstIndex(of: item)
+                                NavigationLink {
+                                    BackgroundView()
+                                } label: {
+                                    HStack {
+                                        Text("Inventory #\(num!)")
+                                            .foregroundColor(.white)
+                                            .padding(.horizontal)
+                                        Spacer()
+                                    }
+                                    .frame(height: 35)
+                                }
+                                // Draws a background stroke around each list item
+                                .listRowBackground(
+                                    EmptyView()
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke()
+                                        )
+                                        .padding(
+                                            EdgeInsets(
+                                                top: 2,
+                                                leading: 2,
+                                                bottom: 2,
+                                                trailing: 2
+                                            )
+                                        )
+                                )
+                                // Adjusts the position of the title and icon in the row
+                                .listRowInsets(.init(top: 5, leading: 0, bottom: 10, trailing: 20))
+                                // Does something important
+                                .listRowSeparator(.hidden)
+                                .listStyle(.plain)
+                            }
+                            // List modifier to allow swipe to delete
+                            .onDelete(perform: deleteItems)
+                        }
+                        // Prevents List style from overriding background
+                        .scrollContentBackground(.hidden)
+                    }
+                }
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        floatingActionButton
+                    }
+                }
+            }
+            .navigationTitle("Offline Inventories")
+        }
+        .tint(primaryLightColor)
     }
 
     private func addItem() {

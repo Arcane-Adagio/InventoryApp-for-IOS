64d6d4937b1d694c677624d25d1cf34353c5d248
Patrick Coleman <patrick.coleman@teambespin.us>
2023-04-27 10:08:43 -0500

    Add second panel and collapse Ability in VehicleDetail

--- InventoryApp/Views/Vehicles/VehicleDetailView.swift
+++ InventoryApp/Views/Vehicles/VehicleDetailView.swift
@@ -13,12 +13,14 @@ struct VehicleDetailView: View {
     @State var editMode = false
     @State var selectedNote = NoteSection.general
     @State var showingDefaultAttributes = true
+    @State var selectedYear: Int
+    @State var collapse = true
     let charLengthVIN = 17
-    @Environment(\.dismiss) var dismiss
 
     init(vehicle: VehicleItem) {
         self._vehicle = StateObject(wrappedValue: vehicle)
         self._selectedDate = State(initialValue: DateFormatter.formate.date(from: vehicle.tagExp) ?? Date())
+        self._selectedYear = State(initialValue: Int(vehicle.year) ?? 2_023)
     }
 
     struct AttributeLabel: View {
@@ -34,40 +36,32 @@ struct VehicleDetailView: View {
         }
     }
 
-    private var editOverlay: some View {
-        HStack {
-            Spacer()
-            Color.white
-                .frame(width: 200, height: 35)
-                .background(.black)
-                .cornerRadius(10)
-        }
-        .opacity(0.1)
-        .offset(x: 10)
-    }
-
     var body: some View {
         ZStack {
             LinearGradient(gradient:
                             Gradient(colors: [Color(hex: "7550bc"), Color(hex: "1c067d")]),
                            startPoint: .topLeading, endPoint: .bottomLeading)
             VStack {
-                headerView
-                Color.white
-                    .frame(height: 0.3)
-                    .padding(.horizontal)
-                    .padding(.bottom, 20)
+                DetailHeaderView(headerText: "\(vehicle.year) \(vehicle.make) \(vehicle.model)",
+                                 $editMode, $showingDefaultAttributes, $collapse)
+                if !collapse {
+                    Color.white
+                        .frame(height: 0.3)
+                        .padding(.horizontal)
+                        .padding(.bottom, 20)
                     VStack {
                         if showingDefaultAttributes {
-                            attributesView
+                            Attribute1Panel(_vehicle, editMode, $selectedDate)
                         } else {
-                            editVehicleView
+                            Attribute2Panel(_vehicle, editMode, $selectedYear)
                         }
                         Spacer()
                             .frame(height: 30)
-                        notesView
-                        Spacer()
                     }
+                }
+                VehicleNotesView(_vehicle, editMode, $selectedNote)
+                    .padding(.top, collapse ? -20 : 0)
+                Spacer()
             }
             .padding(.vertical)
             .padding(.top, 20)
@@ -76,130 +70,232 @@ struct VehicleDetailView: View {
         .navigationBarHidden(true)
     }
 
-    private var attributesView: some View {
-        VStack {
-            HStack {
-                AttributeLabel("VIN:")
-                Spacer()
-                TextField("VIN", text: $vehicle.vin)
-                    .onChange(of: vehicle.vin) { newVin in
-                        self.vehicle.vin = String(newVin.uppercased().prefix(17))
+    struct Attribute1Panel: View {
+        @StateObject var vehicle: VehicleItem
+        var editMode: Bool
+        @Binding var selectedDate: Date
+
+        init(_ vehicle: StateObject<VehicleItem>, _ editMode: Bool, _ selectedDate: Binding<Date>) {
+            self.editMode = editMode
+            self._vehicle = vehicle
+            self._selectedDate = selectedDate
+        }
+
+        var body: some View {
+            VStack {
+                HStack {
+                    AttributeLabel("VIN:")
+                    Spacer()
+                    TextField("VIN", text: $vehicle.vin)
+                        .onChange(of: vehicle.vin) { newVin in
+                            self.vehicle.vin = String(newVin.uppercased().prefix(17))
+                        }
+                    // Only allow textField editing while in Edit Mode
+                        .disabled(!editMode)
+                    // Align the textfield to the right of the view
+                        .multilineTextAlignment(.trailing)
+                    // set the font to look nice
+                        .font(.system(size: 16, weight: .none, design: .rounded))
+                        .background(editMode ? editOverlay : nil)
+                        .padding(.trailing, 10)
+                }
+                .padding(.horizontal)
+                HStack {
+                    MyDatePicker(label: "Tag Expiration:", date: $selectedDate) { date in
+                        vehicle.tagExp = DateFormatter.formate.string(from: date)
                     }
-                // Only allow textField editing while in Edit Mode
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
                     .disabled(!editMode)
-                // Align the textfield to the right of the view
-                    .multilineTextAlignment(.trailing)
-                // set the font to look nice
-                    .font(.system(size: 16, weight: .none, design: .rounded))
-                    .background(editMode ? editOverlay : nil)
-                    .padding(.trailing, 10)
-            }
-            .padding(.horizontal)
-            HStack {
-                MyDatePicker(label: "Tag Expiration", date: $selectedDate) { date in
-                    vehicle.tagExp = DateFormatter.formate.string(from: date)
+                    .padding(.bottom, 3)
                 }
-                .font(.system(size: 16, weight: .bold, design: .rounded))
-                .disabled(!editMode)
-                .padding(.bottom, 3)
+                .padding(.horizontal)
+                HStack {
+                    AttributeLabel("Color:")
+                    Spacer()
+                    TextField("Color", text: $vehicle.color)
+                    // Only allow textField editing while in Edit Mode
+                        .disabled(!editMode)
+                    // Align the textfield to the right of the view
+                        .multilineTextAlignment(.trailing)
+                    // set the font to look nice
+                        .font(.system(size: 16, weight: .none, design: .rounded))
+                        .background(editMode ? editOverlay : nil)
+                        .padding(.trailing, 10)
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+        }
+    }
+
+    struct VehicleNotesView: View {
+        @StateObject var vehicle: VehicleItem
+        var editMode: Bool
+        @Binding var selectedNote: NoteSection
+
+        init(_ vehicle: StateObject<VehicleItem>, _ editMode: Bool, _ selectedNote: Binding<NoteSection>) {
+            self.editMode = editMode
+            self._vehicle = vehicle
+            self._selectedNote = selectedNote
+        }
+
+        var body: some View {
+            VStack {
+                MyNotePicker(date: $selectedNote)
+                    .padding(.horizontal, 40)
+                    .padding(.top, 30)
+                Color.white
+                    .frame(height: 0.3)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 5)
+                ScrollView {
+                    switch selectedNote {
+                    case NoteSection.mechanical:
+                        TextField(editMode ? "Enter notes here" : "Nothing from the mechanic",
+                                  text: $vehicle.mechNotes, axis: .vertical)
+                        .disabled(!editMode)
+                        .font(.system(size: 14, weight: .light, design: .rounded))
+                        .lineLimit(nil)
+                        .padding(.horizontal)
+                    default:
+                        TextField(editMode ? "Enter notes here" : "No notes to show",
+                                  text: $vehicle.genNotes, axis: .vertical)
+                        .disabled(!editMode)
+                        .font(.system(size: 14, weight: .light, design: .rounded))
+                        .lineLimit(nil)
+                        .padding(.horizontal)
+                    }
+                }
+                .frame(height: 250)
+            }
+            .padding(.horizontal, 10)
+        }
+    }
+
+    struct DetailHeaderView: View {
+        @Environment(\.dismiss) var dismiss
+        var headerText: String
+        @Binding var editMode: Bool
+        @Binding var showingFirstPanel: Bool
+        @Binding var collapse: Bool
+
+        init(headerText: String, _ editMode: Binding<Bool>, _ showingFirstPanel: Binding<Bool>,
+             _ collapse: Binding<Bool>) {
+            self.headerText = headerText
+            self._editMode = editMode
+            self._showingFirstPanel = showingFirstPanel
+            self._collapse = collapse
+        }
+
+        var body: some View {
             HStack {
-                AttributeLabel("Color:")
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .tint(.white)
+                }
+                .tint(.gray)
                 Spacer()
-                TextField("Color", text: $vehicle.color)
-                // Only allow textField editing while in Edit Mode
-                    .disabled(!editMode)
-                // Align the textfield to the right of the view
-                    .multilineTextAlignment(.trailing)
-                // set the font to look nice
-                    .font(.system(size: 16, weight: .none, design: .rounded))
-                    .background(editMode ? editOverlay : nil)
-                    .padding(.trailing, 10)
+                Button {
+                    withAnimation {
+                        showingFirstPanel.toggle()
+                    }
+                } label: {
+                    Text(headerText)
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                        .multilineTextAlignment(.center)
+                        .padding(editMode ? 10 : 0)
+                        .background(editMode ? editColor : nil)
+                        .cornerRadius(15)
+                }
+                .foregroundColor(.white)
+                .disabled(!editMode)
+                .tint(.white)
+                Button {
+                    withAnimation {
+                        collapse.toggle()
+                    }
+                } label: {
+                    Image(systemName: collapse ? "chevron.down" : "chevron.up")
+                        .resizable()
+                        .frame(width: 20, height: 10)
+                        .tint(primaryLightColor)
+                }
+                .tint(.gray)
+                Spacer()
+                Button {
+                    editMode.toggle()
+                } label: {
+                    Image(systemName: editMode ? "pencil.line" : "pencil.slash")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .tint(editMode ? primaryLightColor : .white)
+                }
+                .tint(.gray)
+            }
+            .padding([.horizontal, .top])
+        }
+    }
+
+    struct Attribute2Panel: View {
+        @StateObject var vehicle: VehicleItem
+        var editMode: Bool
+        @Binding var selectedYear: Int
+
+        init(_ vehicle: StateObject<VehicleItem>, _ editMode: Bool, _ selectedYear: Binding<Int>) {
+            self.editMode = editMode
+            self._vehicle = vehicle
+            self._selectedYear = selectedYear
+        }
+
+        var body: some View {
+            VStack {
+                MyYearPicker(label: "Year:", date: $selectedYear)
+                    .onChange(of: selectedYear) { newYear in
+                        self.vehicle.year = String(newYear)
+                    }
+                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                    .padding(.trailing, -10)
+                    .padding(.top, -7)
+                HStack {
+                    AttributeLabel("Make:")
+                    Spacer()
+                    TextField("Make", text: $vehicle.make)
+                        .disabled(!editMode)
+                        .multilineTextAlignment(.trailing)
+                        .font(.system(size: 16, weight: .none, design: .rounded))
+                        .background(editMode ? editOverlay : nil)
+                }
+                .padding(.trailing, 10)
+                .padding(.bottom, 10)
+                HStack {
+                    AttributeLabel("Model:")
+                    Spacer()
+                    TextField("Model", text: $vehicle.model)
+                        .disabled(!editMode)
+                        .multilineTextAlignment(.trailing)
+                        .font(.system(size: 16, weight: .none, design: .rounded))
+                        .background(editMode ? editOverlay : nil)
+                        .padding(.trailing, 10)
+                }
             }
             .padding(.horizontal)
         }
     }
+}
 
-    private var notesView: some View {
-        VStack {
-            MyNotePicker(date: $selectedNote)
-                .padding(.horizontal, 40)
-                .padding(.top, 30)
-            Color.white
-                .frame(height: 0.3)
-                .padding(.horizontal, 15)
-                .padding(.top, 5)
-            ScrollView {
-                switch selectedNote {
-                case NoteSection.mechanical:
-                    TextField(editMode ? "Enter notes here" : "Nothing from the mechanic",
-                              text: $vehicle.mechNotes, axis: .vertical)
-                    .disabled(!editMode)
-                    .font(.system(size: 14, weight: .light, design: .rounded))
-                    .lineLimit(nil)
-                    .padding(.horizontal)
-                default:
-                    TextField(editMode ? "Enter notes here" : "No notes to show",
-                              text: $vehicle.genNotes, axis: .vertical)
-                    .disabled(!editMode)
-                    .font(.system(size: 14, weight: .light, design: .rounded))
-                    .lineLimit(nil)
-                    .padding(.horizontal)
-                }
-            }
-            .frame(height: 250)
-        }
-        .padding(.horizontal, 10)
+private var editOverlay: some View {
+    HStack {
+        Spacer()
+        Color.white
+            .frame(width: 200, height: 35)
+            .background(.black)
+            .cornerRadius(10)
     }
-
-    private var headerView: some View {
-        HStack {
-            Button {
-                dismiss()
-            } label: {
-                Image(systemName: "xmark")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .tint(.white)
-            }
-            .tint(.gray)
-            Spacer()
-            Button {
-                withAnimation {
-                    showingDefaultAttributes.toggle()
-                }
-            } label: {
-                Text("\(vehicle.year) \(vehicle.make) \(vehicle.model)")
-                    .font(.system(size: 20, weight: .bold, design: .rounded))
-                    .multilineTextAlignment(.center)
-                    .padding(editMode ? 10 : 0)
-                    .background(editMode ? editColor : nil)
-                    .cornerRadius(15)
-            }
-            .foregroundColor(.white)
-            .disabled(!editMode)
-            .tint(.white)
-            Spacer()
-            Button {
-                editMode.toggle()
-            } label: {
-                Image(systemName: editMode ? "pencil.line" : "pencil.slash")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .tint(editMode ? primaryLightColor : .white)
-            }
-            .tint(.gray)
-        }
-        .padding([.horizontal, .top])
-    }
-
-    // TODO: this
-    private var editVehicleView: some View {
-        VStack {
-            Text("To Do")
-        }
-    }
+    .opacity(0.1)
+    .offset(x: 10)
 }
 
 private var editColor: some View {
